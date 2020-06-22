365dd2fb0b278ff01b24ad334704d1fb9a544efc
Michael Gunawan <gunawan.michael20@gmail.com>
2020-06-22 20:57:11 +0700

    add infinityscroll

--- src/components/Post/PostList.tsx
+++ src/components/Post/PostList.tsx
@@ -2,41 +2,78 @@ import React from "react";
 import firebase from "../../firebase/firebase";
 import PostItem from "./PostItem";
 import TopBaseComponent from "../Commons/TopBaseComponent";
+import {
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  useIonViewWillEnter,
+} from "@ionic/react";
 
 const PostList: React.FC<any> = (props: any) => {
-  const [posts, setPosts] = React.useState([]);
+  const [posts, setPosts] = React.useState<any>([]);
+  const [disableScroll, setDisableScroll] = React.useState(false);
+  const [lastVisible, setLastVisible] = React.useState(null);
   const isPopular = props.location.pathname.includes("trending");
+  const limit = 5;
+
+  useIonViewWillEnter(async () => {
+    await getPosts();
+  });
 
   React.useEffect(() => {
-    const unsubscribe = getLinks();
-    return () => unsubscribe();
+    getPosts();
   }, [isPopular]);
 
-  function getLinks() {
+  async function getPosts() {
     if (isPopular) {
+      if (lastVisible != null) {
+        return firebase.db
+          .collection("posts")
+          .orderBy("voteCount", "desc")
+          .startAfter(lastVisible)
+          .limit(limit)
+          .onSnapshot(handleSnapshot);
+      }
       return firebase.db
         .collection("posts")
         .orderBy("voteCount", "desc")
+        .limit(limit)
         .onSnapshot(handleSnapshot);
     }
+
+    if (lastVisible != null) {
+      return firebase.db
+        .collection("posts")
+        .orderBy("created", "desc")
+        .startAfter(lastVisible)
+        .limit(limit)
+        .onSnapshot(handleSnapshot);
+    }
+
     return firebase.db
       .collection("posts")
       .orderBy("created", "desc")
+      .limit(limit)
       .onSnapshot(handleSnapshot);
   }
 
   function handleSnapshot(snapshot: any) {
-    const posts = snapshot.docs.map((doc: any) => {
+    const freshPosts = snapshot.docs.map((doc: any) => {
       return { id: doc.id, ...doc.data() };
     });
-    setPosts(posts);
+    setLastVisible(snapshot.docs[snapshot.docs.length - 1].data().created);
+    setDisableScroll(freshPosts.length < limit);
+    if (posts.length === 0) {
+      setPosts(freshPosts);
+    } else {
+      //setPosts((posts: any[]) => [...posts, freshPosts]);
+      setPosts((posts: any[]) => posts.concat(freshPosts));
+    }
   }
 
   return (
     <TopBaseComponent>
       {posts.map((post: any, index: number) => {
         var url = `/post/${post.id}`;
-        console.log(index);
         return (
           <PostItem
             key={post.id}
@@ -47,6 +84,13 @@ const PostList: React.FC<any> = (props: any) => {
           />
         );
       })}
+      <IonInfiniteScroll
+        threshold={"100px"}
+        disabled={disableScroll}
+        onIonInfinite={async () => await getPosts()}
+      >
+        <IonInfiniteScrollContent loadingText="Loading..."></IonInfiniteScrollContent>
+      </IonInfiniteScroll>
     </TopBaseComponent>
   );
 };
